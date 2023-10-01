c5b779e9d4ba6c37cb38590d17f62a50a8dfac97
dannyswolf <57114323+dannyswolf@users.noreply.github.com>
2023-10-01 13:17:01 +0300

    Create credentials.py

---
+++ credentials.py
@@ -0,0 +1,11 @@
+secrets = {
+   'url' : 'Django_Server_URL:port/weight/add/', # Βάλε τα δικά σου
+   'ssid' : 'Wifi_SSID', # Βάλε τα δικά σου
+   'password' : 'Wifi_password', # Βάλε τα δικά σου
+   'timezone' : 'Europe/Athens',
+   'mqtt_username' : 'HiveMq_Publish_username', # Βάλε τα δικά σου
+   'mqtt_key' : 'HiveMq_Publish_password', # Βάλε τα δικά σου
+   'broker' : 'HiveMq_url.s1.eu.hivemq.cloud', # Βάλε τα δικά σου
+   'port' : 8883,
+   'DEBUG' : False, # True για εκτυπωσεις στην κονσολα του Thony
+}
